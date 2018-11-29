0a15575e94e31152be7e69d6dd6d4c852d74ee64
William McAdams <musicpulpite@gmail.com>
2018-11-29 18:00:25 -0500

    Bare-bones version of game without AI

--- dist/lib/bundle.js
+++ dist/lib/bundle.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./lib/lightCycles.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./lib/light_cycles.js");
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -133,17 +133,95 @@ const drawGrid = (ctx, w, h, step) => {
 
 /***/ }),
 
-/***/ "./lib/lightCycles.js":
-/*!****************************!*\
-  !*** ./lib/lightCycles.js ***!
-  \****************************/
+/***/ "./lib/computer_bike.js":
+/*!******************************!*\
+  !*** ./lib/computer_bike.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+class ComputerBike {
+  constructor(poly, init_vel, prev_points) {
+    this.poly = poly;
+    this.x = 300;
+    this.y = 300;
+    this.vel = init_vel;
+
+    this.prev_points = prev_points;
+
+    this.handleMove();
+  }
+
+  handleMove() {
+    const increment = () => {
+      this.handleVel();
+      this.x += this.vel[0];
+      this.y += this.vel[1];
+
+      const point = svg.createSVGPoint();
+      point.x = this.x;
+      point.y = this.y;
+
+      if (this.prev_points[[this.x, this.y]]) {
+        const svg = document.getElementById('svg');
+        svg.removeChild(this.poly);
+        clearInterval(move);
+        this.clearPositions();
+      } else if ([0, 600].includes(this.x) || [0, 600].includes(this.y)) {
+        const svg = document.getElementById('svg');
+        svg.removeChild(this.poly);
+        clearInterval(move);
+        this.clearPositions();
+      }
+      this.prev_points[[this.x, this.y]] = true;
+
+      this.poly.points.appendItem(point);
+    }
+
+    const move = setInterval(increment, 10);
+  }
+
+  handleVel() {
+    const prob = Math.random();
+
+    if (prob < 0.005) {
+      this.vel = this.vel.reverse();
+      this.vel[0] = -1*this.vel[0];
+      this.vel[1] = -1*this.vel[1];
+    } else if (prob < 0.01) {
+      this.vel = this.vel.reverse();
+    }
+  }
+
+  clearPositions() {
+    Object.values(this.poly.points).forEach((SVGPoint) => {
+      const point = [SVGPoint.x, SVGPoint.y];
+      delete this.prev_points[point];
+    });
+  }
+
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (ComputerBike);
+
+
+/***/ }),
+
+/***/ "./lib/light_cycles.js":
+/*!*****************************!*\
+  !*** ./lib/light_cycles.js ***!
+  \*****************************/
 /*! no exports provided */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _board_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./board.js */ "./lib/board.js");
-/* harmony import */ var _player_bike_2_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./player_bike_2.js */ "./lib/player_bike_2.js");
+/* harmony import */ var _player_bike_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./player_bike.js */ "./lib/player_bike.js");
+/* harmony import */ var _computer_bike_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./computer_bike.js */ "./lib/computer_bike.js");
+
 
 
 
@@ -151,9 +229,14 @@ window.addEventListener('DOMContentLoaded', () => {
   const canvas = document.getElementById('tronGame');
   if (canvas.getContext) {
     let ctx = canvas.getContext('2d');
-
     Object(_board_js__WEBPACK_IMPORTED_MODULE_0__["initBoard"])(canvas, ctx);
-    const player = new _player_bike_2_js__WEBPACK_IMPORTED_MODULE_1__["default"]();
+
+    const prev_points = {};
+    const player = new _player_bike_js__WEBPACK_IMPORTED_MODULE_1__["default"](prev_points);
+    const enemy_1_poly = document.getElementById('enemy_1');
+    const enemy_1 = new _computer_bike_js__WEBPACK_IMPORTED_MODULE_2__["default"](enemy_1_poly, [-1, 0], prev_points);
+    const enemy_2_poly = document.getElementById('enemy_2');
+    const enemy_2 = new _computer_bike_js__WEBPACK_IMPORTED_MODULE_2__["default"](enemy_2_poly, [0, 1], prev_points);
     player.move();
   }
 })
@@ -161,23 +244,23 @@ window.addEventListener('DOMContentLoaded', () => {
 
 /***/ }),
 
-/***/ "./lib/player_bike_2.js":
-/*!******************************!*\
-  !*** ./lib/player_bike_2.js ***!
-  \******************************/
+/***/ "./lib/player_bike.js":
+/*!****************************!*\
+  !*** ./lib/player_bike.js ***!
+  \****************************/
 /*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 class PlayerBike {
-  constructor() {
+  constructor(prev_points) {
     this.player_poly = document.getElementById('player');
     this.x = 300;
     this.y = 300;
     this.vel = [1, 0];
 
-    this.prev_points = {};
+    this.prev_points = prev_points;
 
     this.handleVel();
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: "./lib/lightCycles.js",
+  entry: "./lib/light_cycles.js",
   output: {
   	filename: "./lib/bundle.js"
   },
